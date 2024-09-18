8046961d37d9a300332286bd2ccd3e12a5b79ce6
OmriOmri31 <omriomrinir@gmail.com>
2024-09-18 08:42:22 +0300

    Integrate Firebase authentication into Login, SignUp, LoginForm, and HomeScreen components. firebase.js and firebaseConfig.js were added.

--- RoseApp/src/firebase/firebase.js
+++ RoseApp/src/firebase/firebase.js
@@ -9,3 +9,4 @@ if (!firebase.apps.length) {
 }
 
 export { firebase };
+//
--- RoseApp/src/firebase/firebaseConfig.js
+++ RoseApp/src/firebase/firebaseConfig.js
@@ -8,3 +8,4 @@ export default  {
     appId: "1:326707440146:web:6a3578a9e0a2c41462c6cb",
     measurementId: "G-8VC2VJLB04"
 };
+//
